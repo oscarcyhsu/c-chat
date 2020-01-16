287855ae99adeb776feed6a42bb137f71f53a37b
yang <b05203017@ntu.edu.tw>
2020-01-16 14:39:55 +0800

    modify handle_list

--- client.cpp
+++ client.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <vector>
 #include <stdlib.h>
+#include <vector>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,276 +10,363 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
+using namespace std;
+
+typedef struct
+{
+	int online;
+	int port;
+	char host[50];
+	char name[100];
+
+} client;
+
+vector<client> clis;
+pthread_mutex_t cli_lock = PTHREAD_MUTEX_INITIALIZER;
+
 int reg(int sockfd);
 int sign(int sockfd);
 int bye(int sockfd);
-void handle_list(char * buf,int sockfd,int read_len);
-void* wait_for_pay(void *listenfd){
-    long long lifd = (long long)listenfd;
-    if(listen((int)lifd,10) < 0)
-        perror("listen: ");
-
-}
-
-int main(int argc,char **argv)
+void handle_list(char *buf, int sockfd, int read_len);
+int send_msg(int sockfd, char username[], char content[]);
+void *wait_for_pay(void *listenfd)
 {
-	if(argc != 3)
-	{
-		printf("Error, enter [ip] [port]\n");
-		exit(0);
-	}
+   long long lifd = (long long)listenfd;
+   if (listen((int)lifd, 10) < 0)
+      perror("listen: ");
+}
 
-	printf("Conneting with the server, please wait.\n");
-	int sockfd;
-	
-	struct sockaddr_in addr;
+int main(int argc, char **argv)
+{
+   if (argc != 3)
+   {
+      printf("Error, enter [ip] [port]\n");
+      exit(0);
+   }
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(argv[2]));
-	addr.sin_addr.s_addr = inet_addr(argv[1]);
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket error:");
-		exit(0);
-	}
-	if(connect(sockfd,(struct sockaddr *)&addr,sizeof(struct sockaddr)) < 0)
-	{
-		perror("Connetion error:");
-		exit(0);
-	}
-	int key = 1;
-	char input[100],buf[1000];
-	if(read(sockfd,buf,1000) < 0){
-		perror("connetion error:");
-	}
-		
-	
+   printf("Conneting with the server, please wait.\n");
+   int sockfd;
 
-	
-	while(key)
-	{
-		printf("enter the number (0:register,1:sign in):\n");
-		scanf("%s",input);
-		if(strlen(input)!=1 || (input[0] != '0' && input[0] != '1')){
-			printf("enter the number in the right way(0:register, 1:sign in)!!!\n");
-		}
-		else if(input[0] == '0')
-			key = reg(sockfd);
-		else if(input[0] == '1')
-			key = sign(sockfd);
+   struct sockaddr_in addr;
 
-		
-	}
-	printf("Enter (Q: quit, U: update the list):\n");
-    while(scanf("%s",input)){
-        if(strlen(input) == 1)
-        {
-            
-            if(input[0]=='Q')
-                bye(sockfd);
-            else if(input[0] == 'U'){
-                sprintf(buf,"List\n");
-                if(write(sockfd,buf,strlen(buf)) <= 0){perror("error while writing to server:");exit(0);}
-				int read_len;
-                if((read_len = read(sockfd,buf,1000)) <= 0){perror("Connection abort:");exit(0);}
-                handle_list(buf,sockfd,read_len);
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(atoi(argv[2]));
+   addr.sin_addr.s_addr = inet_addr(argv[1]);
+   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+   {
+      perror("socket error:");
+      exit(0);
+   }
+   if (connect(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr)) < 0)
+   {
+      perror("Connetion error:");
+      exit(0);
+   }
+   int key = 1;
+   char input[100], buf[1000];
+   if (read(sockfd, buf, 1000) < 0)
+   {
+      perror("connetion error:");
+   }
 
+   while (key)
+   {
+      printf("enter the number (0:register,1:sign in):\n");
+      scanf("%s", input);
+      if (strlen(input) != 1 || (input[0] != '0' && input[0] != '1'))
+      {
+         printf("enter the number in the right way(0:register, 1:sign in)!!!\n");
+      }
+      else if (input[0] == '0')
+         key = reg(sockfd);
+      else if (input[0] == '1')
+         key = sign(sockfd);
+   }
+   printf("Enter (Q: quit, U: update the list, M: message):\n");
+   while (scanf("%s", input))
+   {
+      if (input[0] == 'Q')
+         bye(sockfd);
+      else if (input[0] == 'U')
+      {
+         sprintf(buf, "List\n");
+         if (write(sockfd, buf, strlen(buf)) <= 0)
+         {
+            perror("error while writing to server:");
+            exit(0);
+         }
+         int read_len;
+         if ((read_len = read(sockfd, buf, 1000)) <= 0)
+         {
+            perror("Connection abort:");
+            exit(0);
+         }
+         printf("read:<%s>, readlen:<%d>", buf, read_len);
+         handle_list(buf, sockfd, read_len);
+      }
+      else if( input[0] == 'M'){
+         //send to user
+         char username[50], content[1000];
+         char* p;
+         strtok(input,"-");
+         p = strtok(NULL,"-");
+         printf("p1:%s\n", p);
+         if (p == NULL){ printf("format: M-username-conetent\n"); continue;}
+         else{ sprintf(username ,"%s\0", p);}
+
+         p = strtok(NULL, "-");
+         printf("p2:%s\n", p);
+         if (p == NULL){ printf("format: M-username-conetent\n"); continue;}
+         else{ sprintf(content, "%s\0", p);}
+         send_msg(sockfd, username, content);
+      }
+      printf("Enter (Q: quit, U: update the list, M: message):\n");
+   }
+}
+
+int reg(int sockfd)
+{
+   char input[50];
+   int key = 1;
+
+   while (key)
+   {
+      printf("Enter the name you want to register(2~20 number or alphebat):\n");
+      scanf("%s", input);
+      int i, len = strlen(input);
+      if (len >= 2 && len <= 50)
+      {
+         key = 0;
+         for (i = 0; i < len; i++)
+         {
+            if (!(input[i] >= '0' && input[i] <= '9' ||
+                  input[i] >= 'a' && input[i] <= 'z' ||
+                  input[i] >= 'A' && input[i] <= 'Z'))
+            {
+               key = 1;
+               printf("number or alphetbat only\n\n");
+               break;
             }
-        }
-		printf("Enter (Q: quit, U: update the list):\n");
-    }
+         }
+      }
+      else
+         printf("2~20 number or char\n\n");
+   }
+   char buf[100];
+   sprintf(buf, "REGISTER#%s\n", input);
 
+   if (write(sockfd, buf, strlen(buf)) < 0)
+   {
+      perror("connection abort:");
+      exit(0);
+   }
 
-
-
+   if (read(sockfd, buf, 100) <= 0)
+   {
+      perror("server abort:");
+      exit(0);
+   }
+   if (buf[0] == '1')
+   {
+      printf("register success\n\n");
+   }
+   else
+   {
+      printf("register failed\n\n");
+   }
+   return 1;
 }
 
-int reg(int sockfd){
-	char input[50];
-	int key = 1;
+int sign(int sockfd)
+{
 
-	while(key)
-	{
-		printf("Enter the name you want to register(2~20 number or alphebat):\n");
-		scanf("%s",input);
-		int i,len = strlen(input);
-		if(len >= 2 && len <= 50)
-		{
-			key = 0;
-			for(i =0;i < len;i++)
-			{
-				if(!(input[i]>='0' && input[i] <= '9' ||
-					 input[i] >= 'a' && input[i] <= 'z' ||
-					 input[i] >= 'A' && input[i] <= 'Z'))
-				{
-					key = 1;
-					printf("number or alphetbat only\n\n");
-					break;
-				}
-			}
-		}
-		else
-			printf("2~20 number or char\n\n");
-	}
-	char buf[100];
-	sprintf(buf,"REGISTER#%s\n",input);
+   char input[50];
 
-	if(write(sockfd,buf,strlen(buf))<0){
-		perror("connection abort:");
-		exit(0);
-	}
-	
-	if(read(sockfd,buf,100) <= 0){
-		perror("server abort:");
-		exit(0);
-	}
-	if(buf[0] == '1'){
-		printf("register sucess\n\n");
-	}
-	else{
-		printf("register failed\n\n");
-	}
-	return 1;
+   int key = 1;
+   while (key)
+   {
+      printf("Enter your name(2~20 number or alphebat):\n");
+      scanf("%s", input);
+      int i, len = strlen(input);
 
-}
-
-int sign(int sockfd){
-	
-	
-	
-    char input[50];
-
-    int key = 1;
-	while(key)
-	{
-		printf("Enter your name(2~20 number or alphebat):\n");
-		scanf("%s",input);
-		int i,len = strlen(input);
-        
-		if(len >= 2 && len <= 50)
-		{
-			key = 0;
-			for(i =0;i < len;i++)
-			{
-				if(!(input[i]>='0' && input[i] <= '9' ||
-					 input[i] >= 'a' && input[i] <= 'z' ||
-					 input[i] >= 'A' && input[i] <= 'Z'))
-				{
-					key = 1;
-					printf("number or alphetbat only!!\n\n");
-					break;
-				}
-			}
-		}
-		else
-			printf("2~20 number or char!\n\n");
-	}
-    key = 1;
-    char port[20];
-    while(key){
-        printf("Enter the port number(1024~65535):\n");
-        scanf("%s",port);
-        if(strlen(port)<6){
-            key = atoi(port);
-            if(key>=1024 && key <= 65535){
-                key = 0;
-                continue;
+      if (len >= 2 && len <= 50)
+      {
+         key = 0;
+         for (i = 0; i < len; i++)
+         {
+            if (!(input[i] >= '0' && input[i] <= '9' ||
+                  input[i] >= 'a' && input[i] <= 'z' ||
+                  input[i] >= 'A' && input[i] <= 'Z'))
+            {
+               key = 1;
+               printf("number or alphetbat only!!\n\n");
+               break;
             }
-            key = 1;
-        }
-        printf("port number must be 1024 ~ 65535\n");
-    }
-	struct sockaddr_in addr;
+         }
+      }
+      else
+         printf("2~20 number or char!\n\n");
+   }
+   key = 1;
+   char port[20];
+   while (key)
+   {
+      printf("Enter the port number(1024~65535):\n");
+      scanf("%s", port);
+      if (strlen(port) < 6)
+      {
+         key = atoi(port);
+         if (key >= 1024 && key <= 65535)
+         {
+            key = 0;
+            continue;
+         }
+         key = 1;
+      }
+      printf("port number must be 1024 ~ 65535\n");
+   }
+   struct sockaddr_in addr;
 
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(port));
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(atoi(port));
+   addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+   int listenfd;
+   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+   {
+      perror("socket error:");
+      exit(0);
+   }
+   if (bind(listenfd, (struct sockaddr *)&addr, sizeof(struct sockaddr)) < 0)
+   {
+      perror("bind:");
+      exit(0);
+   }
+   pthread_t pt;
+   pthread_create(&pt, NULL, wait_for_pay, (void *)listenfd);
 
-	int listenfd;
-    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror("socket error:");
-		exit(0);
-	}
-	if(bind(listenfd,(struct sockaddr *)&addr, sizeof(struct sockaddr))< 0 ){perror("bind:"); exit(0);}
-	pthread_t pt;
-	pthread_create(&pt,NULL,wait_for_pay,(void*)listenfd);
+   char buf[1000];
+   sprintf(buf, "S#%s#%s\n", input, port);
 
-    char buf[1000];
-    sprintf(buf,"%s#%s\n",input,port);
-	
-    if(write(sockfd,buf,strlen(buf)) < 0){
-        perror("Error while sending to server:");
-        exit(0);
-    }
-	
-	int read_len;
+   if (write(sockfd, buf, strlen(buf)) < 0)
+   {
+      perror("Error while sending to server:");
+      exit(0);
+   }
 
-    if((read_len = read(sockfd,buf,1000)) <= 0){perror("Connection abort:");exit(0);}
-    
-	printf("readlen %d\n",read_len);
-	
-    if(strstr(buf,"220 AUTH_FAIL") == NULL)
-    {
-		handle_list(buf,sockfd,read_len);
-        return 0;
-    }
-	else
-	{
-		pthread_cancel(pt);
-		close(listenfd);
-		printf("No this user\n\n");
-		return 1;
-	}
+   int read_len;
 
+   if ((read_len = read(sockfd, buf, 1000)) <= 0)
+   {
+      perror("Connection abort:");
+      exit(0);
+   }
+
+   printf("readlen %d\n", read_len);
+
+   if (strstr(buf, "220 AUTH_FAIL") == NULL)
+   {
+      handle_list(buf, sockfd, read_len);
+      return 0;
+   }
+   else
+   {
+      pthread_cancel(pt);
+      close(listenfd);
+      printf("No this user\n\n");
+      return 1;
+   }
 }
-void handle_list(char *buf,int sockfd,int read_len){
-    char *balance = strtok(buf,"\n");
-	int tmp = strlen(balance) + 1;
-    printf("Your balance: %s\n",balance);
-	if(tmp == read_len){
-		if((read_len = read(sockfd,buf,1000)) <= 0){perror("Connection abort:");exit(0);}
-		tmp = 0;
-	}
-	
-    char *ac = strtok(buf+tmp,"\n");
-	tmp += strlen(ac) + 1;
-    int ac_num = atoi(ac+27);
-    int i;
-    char *user,*userip,*userport;
-    printf("%d user(s) online\n",ac_num);
-    for(i=0;i<ac_num;i++){
-		if(tmp == read_len){
-			if((read_len = read(sockfd,buf,1000)) <= 0){perror("Connection abort:");exit(0);}
-			tmp = 0;
+void handle_list(char *buf, int sockfd, int read_len)
+{
+
+	int tmp = 0;
+	clis.clear();
+
+	char *ac = strtok(buf + tmp, "\n");
+	tmp += strlen(ac) + 1; // let tmp = readlen?
+	int ac_num = atoi(ac + 27);
+	int i;
+	char *user, *userip, *userport;
+	printf("%d user(s) in system\n", ac_num);
+	for (i = 0; i < ac_num; i++)
+	{
+		client cli;
+      printf("tmp/readlen:%d/%d\n", tmp, read_len);
+		if (tmp == read_len)
+		{
+         if ((read_len = read(sockfd, buf, 1000)) <= 0)
+         {
+            perror("Connection abort:");
+            exit(0);
+         }
+         tmp = 0;
+         printf("buf:%s\n", buf);
 		}
-        user = strtok(buf + tmp,"#");
-		tmp += strlen(user)+1;
-        
-        userip = strtok(NULL,"#");
+
+		user = strtok(buf + tmp, "#");
+		strcpy(cli.name, user);
+		tmp += strlen(user) + 1;
+		printf("user %s\n", cli.name);
+
+		int online = atoi(strtok(NULL, "#"));
+		cli.online = online;
+		tmp += 2;
+		printf("online %d\n", cli.online);
+
+		userip = strtok(NULL, "#");
 		tmp += strlen(userip) + 1;
+		strcpy(cli.host, userip);
+		printf("ip %s\n", cli.host);
 
-        userport = strtok(NULL,"\n");
+		userport = strtok(NULL, "\n");
 		tmp += strlen(userport) + 1;
+		cli.port = atoi(userport);
+		clis.push_back(cli);
+		printf("port %d\n", cli.port);
 
-        printf("User %d\nUser name : %s\nUser IP : %s\nUser port : %s\n\n",i,user,userip,userport);
+		printf("User %d\nUser name : %s\nUser IP : %s\nUser port : %d\n\n", clis.size() - 1, clis[i].name, clis[i].host, clis[i].port);
 	}
-
 }
 
-int bye(int sockfd){
-	char buf[500];
-	sprintf(buf,"Exit\n");
+int bye(int sockfd)
+{
+   char buf[500];
+   sprintf(buf, "Exit\n");
 
-	if(write(sockfd,buf,strlen(buf)) <= 0){perror("error while writing to server:");exit(0);}
-    if(read(sockfd,buf,1000) <= 0){perror("Connection abort:");exit(0);}
-	
-	if(strstr(buf,"Bye")!= NULL)
-		exit(0);
-	else{
-		printf("error get %s from server while exit\n",buf);
-		exit(0);
-	}
+   if (write(sockfd, buf, strlen(buf)) <= 0)
+   {
+      perror("error while writing to server:");
+      exit(0);
+   }
+   if (read(sockfd, buf, 1000) <= 0)
+   {
+      perror("Connection abort:");
+      exit(0);
+   }
 
+   if (strstr(buf, "Bye") != NULL)
+      exit(0);
+   else
+   {
+      printf("error get %s from server while exit\n", buf);
+      exit(0);
+   }
+}
+
+int send_msg(int sockfd, char username[], char content[]){
+   char buf[1100];
+   sprintf(buf, "L#%s#%s#", username, content);
+   if (write(sockfd, buf, strlen(buf)) <= 0)
+   {
+      perror("error while writing to server:");
+      exit(0);
+   }
+   if (read(sockfd, buf, sizeof(buf)) <= 0)
+   {
+      perror("Connection abort:");
+      exit(0);
+   }
+   return 1;
 }
--- server.cpp
+++ server.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <fcntl.h>
 #include <pthread.h>
@@ -12,201 +13,312 @@
 #include <arpa/inet.h>
 #include <vector>
 
-
 using namespace std;
 
+typedef struct
+{
+   int online, balance;
+   char port[7];
+   char host[50];
+   char name[100];
 
-typedef struct{
-	int online,balance;
-	char port[7];
-	char host[50];
-	char name[100];
-
-}client;
-typedef struct{
-	int fd;
-	char host[50];
-}conn;
+} client;
+typedef struct
+{
+   int fd;
+   char host[50];
+} conn;
 int online_num = 0;
-vector<client> clis;
+vector<client> clis; //clients
 pthread_mutex_t m_lock = PTHREAD_MUTEX_INITIALIZER;
-void* deal_with_client(void *cli);
-int main(int argc, char **argv){
+void *deal_with_client(void *cli);
+int main(int argc, char **argv)
+{
+   int enable = 1;
+   struct stat st = {0};
+   if (argc != 3)
+   {
+      printf("please enter: ./server [port][thread_num]");
+      exit(1);
+   }
 
-	if (argc!=3){
-		printf("please enter: ./server [port][thread_num]");
-		exit(1);
-	}
-	int listenfd,port;
-	int thread_num = atoi(argv[2]);
-	listenfd = socket(AF_INET,SOCK_STREAM,0);
-	if(listenfd < 0){
-		perror("socket()");
-		exit(1);
-	}
-	port = atoi(argv[1]);
-	if (port< 1024 || port > 65535)
-	{
-		printf("port must between 1024 ~ 65535");
-		exit(1);
-	}
-	//addr sets
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if(bind(listenfd,(struct sockaddr*)&addr,sizeof(addr))<0){perror("bind: ");exit(1);}
+   if (stat("log", &st) != 0){ 
+      if(mkdir("log", 0700) < 0){
+         perror("create log folder");
+         exit(-1);
+      }
+   }
+   
+   int listenfd, port;
+   int thread_num = atoi(argv[2]);
+   listenfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (listenfd < 0)
+   {
+      perror("socket()");
+      exit(1);
+   }
+   if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+   {
+      perror("setsockopt(SO_REUSEADDR) failed");
+      exit(1);
+   }
+   port = atoi(argv[1]);
+   if (port < 1024 || port > 65535)
+   {
+      printf("port must between 1024 ~ 65535");
+      exit(1);
+   }
+   //addr sets
+   struct sockaddr_in addr;
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(port);
+   addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   if (bind(listenfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+   {
+      perror("bind: ");
+      exit(1);
+   }
 
-	if(listen(listenfd,5)<0)
-	{
-		perror("listen: ");
-		exit(1);
-	}
-	
-	int newfd;
+   if (listen(listenfd, 5) < 0)
+   {
+      perror("listen: ");
+      exit(1);
+   }
 
-	struct sockaddr_in ac_addr;
-	ac_addr.sin_family = AF_INET;
-	ac_addr.sin_port = htons(port);
-	ac_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	int ac_size = sizeof(ac_addr);
+   int newfd;
 
-	while(1){
-		
-		if((newfd = accept(listenfd,(struct sockaddr*)&ac_addr,(socklen_t *) &ac_size)) < 0 ){
-			perror("accept ");
-			exit(1);
-		}
-		conn *con =(conn*) malloc(sizeof(conn));
-		con->fd = newfd;
-		strcpy(con->host,inet_ntoa(ac_addr.sin_addr));
-		printf("new connection %d, %s\n",newfd,con->host);
-		pthread_t pnum;
-		pthread_create(&pnum,NULL,&deal_with_client,(void *)con);
-		pthread_detach(pnum);
+   struct sockaddr_in ac_addr;
+   ac_addr.sin_family = AF_INET;
+   ac_addr.sin_port = htons(port);
+   ac_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   int ac_size = sizeof(ac_addr);
 
-	}
-
+   while (1)
+   {
+      if ((newfd = accept(listenfd, (struct sockaddr *)&ac_addr, (socklen_t *)&ac_size)) < 0)
+      {
+         perror("accept ");
+         exit(1);
+      }
+      conn *con = (conn *)malloc(sizeof(conn));
+      con->fd = newfd;
+      strcpy(con->host, inet_ntoa(ac_addr.sin_addr));
+      printf("new connection %d, %s\n", newfd, con->host);
+      pthread_t pnum;
+      pthread_create(&pnum, NULL, &deal_with_client, (void *)con);
+      pthread_detach(pnum);
+   }
 }
 
-void* deal_with_client(void *con){
-	int fd = ((conn *) con) -> fd;
-	char host[50];
-	strcpy(host,((conn *) con) -> host);
-	free(con);
+void *deal_with_client(void *con)
+{
+   int fd = ((conn *)con)->fd;
+   char host[50];
+   strcpy(host, ((conn *)con)->host);
+   free(con);
 
-	char buf[1000];
+   char buf[1000];
 
-	//accept
-	sprintf(buf,"connection accepted\n");
-	if(write(fd,buf,strlen(buf))<0){perror("write"); return 0;}
-	//register or sign
-	int i,j,key,user_num = -1;
-	char *name;
-	
-	while (user_num == -1)
-	{
-		int l;
-		if((l = read(fd,buf,sizeof(buf))<= 0 )){perror("read");return 0;}
-		
-		//register
-		if(buf[0] == 'R'){
-			key =1;
-			name = strtok(buf,"#");
-			name = strtok(NULL,"\n");
-			//lock mutex for check
-			if(pthread_mutex_lock(&m_lock) < 0){perror("mutex_lock");return 0;}
-			for(i=0;i<clis.size();i++)
-			{
-				if(strcmp(name,clis[i].name)==0)
-				{
-					key = 0;
-					break;
-				}
-			}
-			if(key){
-				//set new client
-				client cli;
-				cli.online = 0;
-				cli.balance = 1000;
-				strcpy(cli.name,name);
-				
-				clis.push_back(cli);
-				
-				sprintf(buf,"100 OK\n");
-				if(write(fd,buf,strlen(buf))<0) {perror("write");return 0;}
-			}
-			else{
-				sprintf(buf,"210 FAIL\n");
-				if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
-			}
-			//unlock after check and change
-			if(pthread_mutex_unlock(&m_lock)< 0 ){perror("mutex_unlock");return 0;}
-		}
-		else {
-			//sign
-			char *port;
-			name = strtok(buf,"#");
-			port = strtok(NULL,"\n");
-			if(pthread_mutex_lock(&m_lock) < 0){perror("mutex_lock");return 0;}
-			for(i=0;i<clis.size();i++){
-				if(strcmp(clis[i].name,name) == 0){
-					//exist
-					online_num++;
-					user_num = i;
-					clis[i].online = 1;
-					strcpy(clis[i].port,port);
-					strcpy(clis[i].host,host);
-					sprintf(buf,"%d\n",clis[i].balance);
-					if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;};
-					sprintf(buf,"number of accounts online: %d\n",online_num);
-					if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
-					for(j = 0; j<clis.size();j++){
-						if(clis[j].online){
-							sprintf(buf,"%s#%s#%s\n",clis[j].name,clis[j].host,clis[j].port);
-							if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
-						}
-					}
-					break;
-				}
-			}
-			if(pthread_mutex_unlock(&m_lock)< 0 ){perror("mutex_unlock");return 0;}
-			if(user_num == -1){
-				sprintf(buf,"220 AUTH_FAIL\n");
-				if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
-			}
+   //accept
+   sprintf(buf, "connection accepted\n");
+   if (write(fd, buf, strlen(buf)) < 0)
+   {
+      perror("write");
+      return 0;
+   }
+   //register or sign
+   int i, j, key, user_num = -1;
+   char *name;
 
-		}
-	}
-	while(1){
-		if(read(fd,buf,sizeof(buf))<0){perror("read"); return 0;}
-		if(buf[0]=='L')
-		{
-			if(pthread_mutex_lock(&m_lock)< 0 ){perror("mutex_lock");return 0;}
-			sprintf(buf,"%d\n",clis[user_num].balance);
-			if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;};
-			sprintf(buf,"number of accounts online: %d\n",online_num);
+   while (user_num == -1)
+   {
+      int l;
+      if ((l = read(fd, buf, sizeof(buf)) <= 0))
+      {
+         perror("read");
+         return 0;
+      }
+
+      //register
+      if (buf[0] == 'R')
+      {
+         key = 1;
+         name = strtok(buf, "#");
+         name = strtok(NULL, "\n");
+         //lock mutex for check
+         if (pthread_mutex_lock(&m_lock) < 0)
+         {
+            perror("mutex_lock");
+            return 0;
+         }
+         for (i = 0; i < clis.size(); i++)
+         {
+            if (strcmp(name, clis[i].name) == 0)
+            {
+               key = 0;
+               break;
+            }
+         }
+         if (key)
+         {
+            //set new client
+            client cli;
+            cli.online = 0;
+            cli.balance = 1000;
+            strcpy(cli.name, name);
+
+            clis.push_back(cli);
+
+            struct stat st;
+            char logpath[100];
+            sprintf(logpath, "log/%s", cli.name);
+            if (stat(logpath, &st) != 0){ 
+               if(mkdir(logpath, 0700) < 0){
+                  perror("create personal log folder");
+                  exit(-1);
+               }
+            }
+
+            sprintf(buf, "100 OK\n");
+            if (write(fd, buf, strlen(buf)) < 0)
+            {
+               perror("write");
+               return 0;
+            }
+         }
+         else
+         {
+            sprintf(buf, "210 FAIL\n");
+            if (write(fd, buf, strlen(buf)) < 0)
+            {
+               perror("write");
+               return 0;
+            }
+         }
+         //unlock after check and change
+         if (pthread_mutex_unlock(&m_lock) < 0)
+         {
+            perror("mutex_unlock");
+            return 0;
+         }
+      }
+      else if(buf[0] == 'S')//sign in 
+      {
+         //sign
+         char *port;
+         name = strtok(buf+1, "#");
+         port = strtok(NULL, "\n");
+         if (pthread_mutex_lock(&m_lock) < 0)
+         {
+            perror("mutex_lock");
+            return 0;
+         }
+         for (i = 0; i < clis.size(); i++)
+         {
+            if (strcmp(clis[i].name, name) == 0)
+            {
+               //exist
+               online_num++;
+               user_num = i;
+               clis[i].online = 1;
+               strcpy(clis[i].port, port);
+               strcpy(clis[i].host, host);
+               sprintf(buf, "number of accounts online: %d\n", online_num);
+               if (write(fd, buf, strlen(buf)) < 0)
+               {
+                  perror("write");
+                  return 0;
+               }
+               for (j = 0; j < clis.size(); j++)
+               {
+                  if (clis[j].online)
+                  {
+                     sprintf(buf, "%s#%d#%s#%s\n", clis[j].name,clis[j].online, clis[j].host, clis[j].port);
+                     if (write(fd, buf, strlen(buf)) < 0)
+                     {
+                        perror("write");
+                        return 0;
+                     }
+                  }
+               }
+               break;
+            }
+         }
+         if (pthread_mutex_unlock(&m_lock) < 0)
+         {
+            perror("mutex_unlock");
+            return 0;
+         }
+         if (user_num == -1)
+         {
+            sprintf(buf, "220 AUTH_FAIL\n");
+            if (write(fd, buf, strlen(buf)) < 0)
+            {
+               perror("write");
+               return 0;
+            }
+         }
+      }
+   }
+   while (1)
+   {
+      if (read(fd, buf, sizeof(buf)) < 0)
+      {
+         perror("read");
+         return 0;
+      }
+      if (buf[0] == 'L') //online userlist
+      {
+         if(pthread_mutex_lock(&m_lock)< 0 ){perror("mutex_lock");return 0;}
+
+			sprintf(buf,"number of accounts insys : %d\n",clis.size());
 			if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
 			for(j =0; j<clis.size();j++){
-				if(clis[j].online){
-					sprintf(buf,"%s#%s#%s\n",clis[j].name,clis[j].host,clis[j].port);
+				if(1){
+					sprintf(buf,"%s#%d#%s#%s\n", clis[j].name, clis[j].online, clis[j].host, clis[j].port);
 					if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;}
 				}
 			}
 			if(pthread_mutex_unlock(&m_lock)< 0 ){perror("mutex_unlock");return 0;}
-		}
-		else if(buf[0] == 'E'){
-			sprintf(buf,"Bye\n");
-			if(pthread_mutex_lock(&m_lock)< 0 ){perror("mutex_lock");return 0;}
-			online_num--;
-			clis[user_num].online = 0;
-			if(pthread_mutex_unlock(&m_lock)< 0 ){perror("mutex_unlock");return 0;}
-			if(write(fd,buf,strlen(buf)) < 0 ){ perror("write"); return 0;};
-			break;
+      }
+      else if (buf[0] == 'E')
+      {
+         //log out
+         sprintf(buf, "Bye\n");
+         if (pthread_mutex_lock(&m_lock) < 0)
+         {
+            perror("mutex_lock");
+            return 0;
+         }
+         online_num--;
+         clis[user_num].online = 0;
+         if (pthread_mutex_unlock(&m_lock) < 0)
+         {
+            perror("mutex_unlock");
+            return 0;
+         }
+         if (write(fd, buf, strlen(buf)) < 0)
+         {
+            perror("write");
+            return 0;
+         }
+         close(fd);
+         break;
+      }
+      else if (buf[0] == 'M')
+      {
+         char *p_username, *p_content;
+         strtok(buf, "#");
+         p_username = strtok(NULL, "#");
+         p_content = strtok(NULL, "#");
+         for (j = 0; j < clis.size(); j++)
+         {
 
-		}
-	}
-	return 0;
-
-
+            if (clis[j].online)
+            {
+            }
+         }
+      }
+   }
+   return 0;
 }
